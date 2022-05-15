56a44316791e6b032aad447d7788353444e573ea
Varun Santhanam <talkto@vsanthanam.com>
2022-05-15 10:56:33 -0700

    Update Documentation Generation Script

--- gen-hosted-docs.sh
+++ gen-hosted-docs.sh
@@ -1,1 +1,6 @@
+git branch -D gh-pages
+git checkout -b gh-pages
 swift package --allow-writing-to-directory docs generate-documentation --target Reachability --disable-indexing --transform-for-static-hosting --hosting-base-path Reachability/docs --output-path docs
+git add .
+git commit -m 'Publish Documentatin'
+git push -f -u origin gh-pages
