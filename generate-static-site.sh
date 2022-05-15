5a9338374029413f712f1b679ae9b406b162c721
Varun Santhanam <talkto@vsanthanam.com>
2022-05-15 11:20:55 -0700

    Update Generation Script

--- generate-static-site.sh
+++ generate-static-site.sh
@@ -6,7 +6,8 @@ if [[ "$branch" != "main" ]]; then
 else
     git branch -D gh-pages
     git checkout -b gh-pages
-    swift package --allow-writing-to-directory docs generate-documentation --target Reachability --disable-indexing --transform-for-static-hosting --hosting-base-path Reachability/docs --output-path docs
+    swift package --allow-writing-to-directory docs generate-documentation --target Reachability --disable-indexing --transform-for-static-hosting --hosting-base-path docs --output-path docs
+    echo "reachability-swift.dev" > CNAME
     git add .
     git commit -m 'Synchronize Hompage & Publish Documentation'
     git push -f -u origin gh-pages
