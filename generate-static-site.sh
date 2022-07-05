74f49cb628383339187b1c864bb6dfabc8034e79
Varun Santhanam <talkto@vsanthanam.com>
2022-07-04 17:41:36 -0700

    Update Site Generator

--- generate-static-site.sh
+++ generate-static-site.sh
@@ -7,7 +7,9 @@ else
     git branch -D gh-pages
     git checkout -b gh-pages
     swift package --allow-writing-to-directory docs generate-documentation --target NetworkReachability --disable-indexing --transform-for-static-hosting --hosting-base-path docs --output-path docs
+    tail -n +2 README.md > README-2.md && mv README-2.md README.md
     echo "reachability.tools" > CNAME
+    echo "theme: jekyll-theme-modernist" > _config.yml
     git add .
     git commit -m 'Synchronize Hompage & Publish Documentation'
     git push -f -u origin gh-pages
